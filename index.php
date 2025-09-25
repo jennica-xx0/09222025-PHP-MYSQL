ecb387147184c5884f1b699370a522f5e4b212ef
jennica-xx0 <jennica3003@gmail.com>
2025-09-25 02:00:56 -0700

    with tailwind css

--- index.php
+++ index.php
@@ -2,43 +2,36 @@
 <html lang="en" class="h-full">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>Register</title>
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
 
-<body class="h-full flex justify-center items-center ">
+<body class="bg-[url('https://upload.wikimedia.org/wikipedia/commons/7/71/Gli-Gli_Barangay_Hall_SGA.jpg')] h-screen w-full bg-cover bg-center bg-no-repeat flex justify-center items-center">
+    <div class="max-y-[60px] bg-white/70 p-4 space-y-4 rounded-2xl flex-row justify-center items-center">
+        <h1 class="justify-self-center font-bold text-2xl font-mono m-6">Create Account</h1>
+        <div>
+            <h1 class="justify-self-center items-center font-semibold">Fullname</h1>
+            <input class="w-full border-2 rounded-md p-1 mt-2" type="text" name="fullname" placeholder="Enter your name" required />
+        </div>
 
-<!-- <div class="space-y-20 border-4 border-red-400 rounded-2xl inset-shadow-sm inset-shadow-indigo-500 m-10 p-6"> -->
-<div class="border-4 border-red-400 rounded-2xl p-6 outline outline-4 outline-indigo-500">       
-<h1 class="justify-self-center font-bold text-2xl font-mono">Create Account</h1>
-        <form class="justify-self-center space-y-5 " 
-            action="register.php" method="POST">
+        <div>
+            <h1 class="justify-self-center items-center px-10 font-semibold">Email</h1>
+            <input class=" w-full rounded-md border-2 p-1" type="email" name="email" placeholder="Enter your email" required />
+        </div>
 
-            <div class="">
-                <h1 class="justify-self-center items-center px-10 font-semibold">Fullname</h1>
-                <input class="border-2 border-spacing-2"
-                    type="text" name="fullname" placeholder="Enter your name" required />
-            </div>
+        <div>
+            <h1 class="justify-self-center items-center px-10 font-semibold">Password</h1>
+            <input class=" w-full border-2 rounded-md p-1 border-spacing-2" type="password" name="password" placeholder="Enter your password" required />
+        </div>
 
-            <div class="">
-                <h1 class="justify-self-center items-center px-10 font-semibold">Email</h1>
-                <input class="border-2 border-spacing-2"
-                    type="text" name="fullname" placeholder="Enter your email" required />
-            </div>
-
-
-            <div class="">
-                <h1 class="justify-self-center items-center px-10 font-semibold">Password</h1>
-                <input class="border-2 border-spacing-2"
-                    type="text" name="fullname" placeholder="Enter your password" required />
-            </div>
-            <button class="justify-self-center items-center px-16 font-semibold"
-                type="submit">Register</button>
+        <div>
+            <button class="justify-self-center items-center px-16 w-full font-semibold" type="submit">Register</button>
+        </div>
         </form>
 
-        <p class="justify-self-center">Already have an account? <a href="index1.php" class="text-red-800">Login here</a></p>
+        <p class="justify-self-center m-4">Already have an account? <a href="index1.php" class="text-red-800">Login here</a></p>
     </div>
 </body>
 
--- index1.php
+++ index1.php
@@ -4,17 +4,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
-<body>
-    <div class="container">
-        <h1>Login</h1>
-        <form action="login.php" method="POST">
-            <input type="email" name="email" placeholder="Enter your email" required />
-            <input type="password" name="password" placeholder="Enter your password" required />
-            <button type="submit">Login</button>
+<body class="h-full flex justify-center items-center">
+    <div class=" border-4 border-red-400 rounded-2xl p-7 outline outline-4 outline-indigo-500 m-30">
+        <h1 class="justify-self-center font-bold text-2xl font-mono m-3 p-2">Login</h1>
+        <form class="justify-self-center -mr-3" action="login.php" method="POST">
+          
+          <div>
+            <h1 class="justify-self-center items-center px-10 font-semibold m-2 mr-5">Email</h1>
+            <input class="border-2 border-spacing-2 mr-2 p-1" type="email" name="email" placeholder="Enter your email" required />
+          </div>
+
+     `    <div>
+             <h1 class="justify-self-center items-center px-10 font-semibold m-2 mr-5">Password</h1>
+            <input class="border-2 border-spacing-1 p-1" type="password" name="password" placeholder="Enter your password" required />
+          </div>
+
+            <button class="justify-self-center items-center font-semibold px-12 m-6" type="submit">Login</button>
         </form>
 
-        <p>Don’t have an account? <a href="index.php">Register here</a></p>
+        <p class="justify-self-center m-3">Don’t have an account? <a href="index.php" class="text-red-800">Register here</a></p>
     </div>
 </body>
 </html>
